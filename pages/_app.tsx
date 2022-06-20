07a64dab3c97209e1722d69e99562bed08ef49c6
davidanitoiu <david.anitoiu@gmail.com>
2022-06-20 02:34:53 +0200

    Moved Next Head to App

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,20 @@ import "@fontsource/permanent-marker";
 import 'styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from "components/Layout";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <title>Ninja Clan Manager</title>
+        <meta name="description" content="Manager type game, inspired by Football Manager, but with Ninjas" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
 
