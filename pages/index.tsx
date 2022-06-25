aca8e09603e1d97a353c30ec06bd031a88a9f1ba
davidanitoiu <david.anitoiu@gmail.com>
2022-06-25 19:53:29 +0200

    Updated landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,11 @@ import Link from 'next/link';
 
 const Home: NextPage = () => {
   return (
-    <div className='grid place-items-center h-screen w-full'>
+    <div className='grid place-content-center gap-4 text-center h-screen w-full font-display'>
+      <h1 className='text-4xl'>Ninja Clan Manager</h1>
+      <h2 className='text-2xl'>It's all under construction. ALL OF IT!</h2>
       <Link href={"/mission"}>
-        <a className='font-display text-black hover:text-primary text-4xl transition-colors'>Go to Mission Dashboard</a>
+        <a className=' text-black hover:text-primary text-4xl transition-colors'>{">> Go to Mission Dashboard <<"}</a>
       </Link>
     </div>
   )
