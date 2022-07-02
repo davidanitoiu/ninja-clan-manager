a04b2f20fed177296e150ea71d96a77b73253aab
davidanitoiu <david.anitoiu@gmail.com>
2022-07-02 19:40:35 +0200

    Added suggestions provided by AI

---
+++ utils/ai/ideasProvidedByAi.ts
@@ -0,0 +1,109 @@
+// things to account for
+// The ninja should account for the guards' apprehension, scouting, hand-to-hand combat, ranged combat, pace, acceleration, and determination when infiltrating the military encampment.
+
+// To calculate the outcome
+//
+// -The ninja would first use their lockpicking attribute to try and open the door to the encampment
+// -If they are unsuccessful, they will use their hand to hand attribute to try and fight their way in
+// -Once inside, they will use their subterfuge skills to try and avoid detection
+// -If they are detected, they will use their combat skills to fight their way out
+
+// Possible scenario
+// The ninja would most likely be successful in infiltrating the military encampment, as their attributes indicate they are skilled in subterfuge and combat. The guards may be able to apprehend the ninja if they are alerted to their presence, but the ninja would likely be able to escape or fight their way out if necessary.
+
+/*
+  -The ninja's chance of success is calculated by adding together all of the relevant attribute scores and dividing by the number of attributes being used.
+  -For example, the ninja's chance of success at hand-to-hand combat would be (7+2+8+5+2+6+9+8)/8 = 50/8 = 6.25.
+  -The guards' chance of success is calculated in the same way.
+  -If the ninja's chance of success is greater than the guards' chance of success, then the ninja will succeed.
+
+
+
+
+-Kiyota Yukio would use his subterfuge.assassination attribute to calculate his chance of success in assassinating the target.
+-His assassination attribute (7) would be compared to the apprehension attribute of the guards (4 and 3).
+-Kiyota Yukio would have a higher chance of success if the guards' apprehension attribute is lower than his assassination attribute.
+
+-The ninja's chance of success is calculated by adding up all of their relevant attribute scores and comparing it to the guards' attribute scores.
+-The ninja's physical attributes (strength, stamina, reflexes, pace, acceleration, agility, balance, precision) are most relevant when infiltrating a military encampment, so those are the attribute scores that should be added up.
+-The guards' attribute scores that are most relevant are their apprehension, scouting, hand-to-hand combat, ranged combat, pace, and acceleration.
+-The ninja's chance of success is calculated by adding up all of their relevant attribute scores and comparing it to the guards' attribute scores. If the ninja's score is higher, they have a higher chance of success.
+
+
+-The ninja will use their lockpicking skills to try and pick the lock of the door to the encampment.
+-If the lock is picked successfully, the ninja will use their stealth skills to try and sneak past the guards.
+-If the ninja is spotted by the guards, they will use their hand-to-hand combat skills to try and fight their way past.
+-If the ninja manages to make it past the guards, they will use their assassination skills to try and kill their target.
+
+-The ninja will attempt to assassinate the target while they are asleep
+-The ninja will try to poison the guard's food
+-The ninja will try to ambush the guard from behind
+-The ninja will try to lure the guard into a trap
+-The ninja will try to pick the lock to the guard's room
+-The ninja will try to create a diversion to draw the guard away from their post
+-The ninja could use their agility to quickly scale the walls of the encampment and take out the guards from above with their precision
+-The ninja could use their subterfuge skills to sneak into the encampment and take out the guards one by one without being detected
+-The ninja could use their hand to hand combat skills to take on the guards head on and take them out in close combat
+-The ninja could use their ranged combat skills to take on the guards from a distance and pick them off one by one
+*/
+
+
+
+//-The ninja's chance of success is calculated by adding up all of their relevant attribute scores and comparing it to the guards' attribute scores.
+//-The ninja's physical attributes (strength, stamina, reflexes, pace, acceleration, agility, balance, precision) are most relevant when infiltrating a military encampment, so those are the attribute scores that should be added up.
+//-The guards' attribute scores that are most relevant are their apprehension, scouting, hand-to-hand combat, ranged combat, pace, and acceleration.
+//-The ninja's chance of success is calculated by adding up all of their relevant attribute scores and comparing it to the guards' attribute scores. If the ninja's score is higher, they have a higher chance of success.
+
+// the ninja's chance of success is calculated by adding up all of their relevant attribute scores and comparing it to the guards' attribute scores. If the ninja's score is higher, they have a higher chance of success.
+// the ninja will use their lockpicking skills to try and pick the lock of the door to the encampment.
+// if the lock is picked successfully, the ninja will use their stealth skills to try and sneak past the guards.
+// if the ninja is spotted by the guards, they will use their hand-to-hand combat skills to try and fight their way past.
+// if the ninja manages to make it past the guards, they will use their assassination skills to try and kill their target.
+// the ninja will attempt to assassinate the target while they are asleep
+// the ninja will try to poison the guard's food
+// the ninja will try to ambush the guard from behind
+// the ninja will try to lure the guard into a trap
+// the ninja will try to pick the lock to the guard's room
+// the ninja will try to create a diversion to draw the guard away from their post
+// the ninja could use their agility to quickly scale the walls of the encampment and take out the guards from above with their precision
+// the ninja could use their subterfuge skills to sneak into the encampment and take out the guards one by one without being detected
+// the ninja could use their hand to hand combat skills to take on the guards head on and take them out in close combat
+// the ninja could use their ranged combat skills to take on the guards from a distance and pick them off one by one
+
+// -The ninja's lockpicking attribute (10) is greater than the guards' apprehension attribute (4), so the ninja will pick the lock and remain stealthed.
+// -The ninja's lockpicking attribute (10) is greater than the guards' scouting attribute (5), so the ninja will pick the lock and remain stealthed.
+// -The ninja's lockpicking attribute (10) is greater than the guards' hand-to-hand attribute (1), so the ninja will pick the lock and remain stealthed.
+// -The ninja's lockpicking attribute (10) is greater than the guards' ranged attribute (6), so the ninja will pick the lock and remain stealthed.
+// -The ninja's lockpicking attribute (10) is greater than the guards' pace attribute (2), so the ninja will pick the lock and remain stealthed.
+// -The ninja's lockpicking attribute (10) is greater than the guards' acceleration attribute (6), so the ninja will pick the lock and remain stealthed.
+// -The ninja's lockpicking attribute (10) is greater than the guards' determination attribute (3), so the ninja will pick the lock and remain stealthed.
+
+
+// The ninja will be successful if:
+// - their handToHand attribute is greater than the guards' handToHand attribute
+// - their ranged attribute is greater than the guards' ranged attribute
+// - their pace attribute is greater than the guards' pace attribute
+// - their acceleration attribute is greater than the guards' acceleration attribute
+// - their stealth attribute is greater than the guards' apprehension attribute
+// - their pickpocketing attribute is greater than the guards' apprehension attribute
+// - their lockpicking attribute is greater than the guards' apprehension attribute
+// - their poison attribute is greater than the guards' determination attribute
+// - their medicine attribute is greater than the guards' determination attribute
+// - their trapMaking attribute is greater than the guards' determination attribute
+
+// 1. The ninja will approach the guards from behind.
+// 2. The ninja will use his lockpicking skills to disable the guards' weapons.
+// 3. The ninja will use his hand-to-hand combat skills to subdue the guards.
+// 4. The ninja will use his assassination skills to kill the guards.
+// 5. The ninja will use his pickpocketing skills to take the guards' keys.
+// 6. The ninja will use his poison skills to incapacitate the guards.
+// 7. The ninja will use his medicine skills to revive the guards.
+// 8. The ninja will use his trap making skills to trap the guards.
+// 9. The ninja will use his mental aggression to intimidate the guards.
+// 10. The ninja will use his mental anticipation to anticipate the guards' movements.
+// 11. The ninja will use his mental decision making to make quick decisions.
+// 12. The ninja will use his mental creativity to come up with new ways to defeat the guards.
+// 13. The ninja will use his mental positioning to position himself in the best position to defeat the guards.
+// 14. The ninja will use his mental scouting to scout the guards' weaknesses.
+// 15. The ninja will use his mental negotiation to negotiate with the guards.
+// 16. The ninja will use his mental influence to influence the guards.
---
+++ utils/ai/templateRequestOpenAI.ts
@@ -0,0 +1,19 @@
+/*
+
+    Open AI request template
+    Temperature: 0.2
+    Maximum Length: 256
+
+    Generate a detailed step-by-step assassination event with risk of dying for this ninja:
+
+    {"personal":{"name":"KiyotaYukio","age":41},"attributes":{"physical":{"strength":7,"stamina":2,"reflexes":8,"pace":5,"acceleration":2,"agility":6,"balance":9,"precision":8},"combat":{"handToHand":7,"ranged":3},"subterfuge":{"stealth":1,"assassination":7,"pickpocketing":6,"lockpicking":10,"poison":8,"medicine":5,"trapMaking":4},"mental":{"aggression":6,"anticipation":7,"decision":5,"creativity":9,"positioning":1,"scouting":3,"negotiation":7,"influence":1}}}
+
+    infiltrating a military encampment with these guards:
+
+    [{"name":"TsutomuHanzo","alive":true,"attributes":{"apprehension":4,"scouting":4,"handToHand":1,"ranged":6,"pace":2,"acceleration":6,"determination":3}},{"name":"KazuyaMishima","alive":true,"attributes":{"apprehension":3,"scouting":5,"handToHand":2,"ranged":6,"pace":5,"acceleration":6,"determination":2}}]
+
+    Using bullet-list format.
+
+  */
+
+// "ninja": {"personal":{"name":"Kiyota Yukio","age":41},"attributes":{"physical":{"strength":7,"stamina":2,"reflexes":8,"pace":5,"acceleration":2,"agility":6,"balance":9,"precision":8},"combat":{"handToHand":7,"ranged":3},"subterfuge":{"stealth":1,"assassination":7,"pickpocketing":6,"lockpicking":10,"poison":8,"medicine":5,"trapMaking":4},"mental":{"aggression":6,"anticipation":7,"decision":5,"creativity":9,"positioning":1,"scouting":3,"negotiation":7,"influence":1}}}
